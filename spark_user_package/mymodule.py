7f58232f76fbef4138105f34df39d1d17be52027
Pradeep Kumar Paladugula <43254499+PradeepKumar1994@users.noreply.github.com>
2020-10-09 19:19:45 -0400

    Update mymodule.py

--- spark_user_package/mymodule.py
+++ spark_user_package/mymodule.py
@@ -84,10 +84,8 @@ class PySpark_con:
         def share_data_flask(self, option):
             
             #changing to dataframes
-            
             pandas_dataframe = self.pyspark_dataframe.toPandas()
-            #df_pandas = querying(self)
-            
+           
             return pandas_dataframe
         
         
@@ -119,7 +117,6 @@ class PySpark_con:
 
 
 
-#
 _spark_ = PySpark_con()
 
 pandas_dataframe = _spark_.data_transformations(option = "timeseries")
@@ -129,137 +126,3 @@ print(pandas_dataframe.iloc[:11, :])
 
 
 
-#.config('spark.jars.packages', 'org.mongodb.spark:mongo-spark-connector_2.11-2.4.2')\
-
-
-
-# To Pandas dataframes
-
-
-#df.show(n=5)#to view the data
-
-#df.printSchema() # to view the schema
-
-
-#
-#print("--- Quering ---")
-#print(df_pandas['timer'].describe())
-
-
-#start_time, end_time, option = (input("Please enter start and end time to query:  ")).split(' ')
-
-#print(start_time,end_time, option)
-
-
-#queryed_time = querying(start = 2, end = 1000, option= 'yes')
-
-
-
-#df_query_time = df_pandas.loc[(df_pandas['time'] >= start_time) and (df_pandas['time'] <= end_time)]
-
-#df_pandas.query('timer >= 1.005')
-
-#print(df_query_time.head(3))
-
-
-#flask_app = flask.Flask(__name__)
-#
-#backup_df = df 
-
-#   backup_df.show(n=10)
-
-#
-#def missing_values():
-#    
-#    temp = df.select(*(sum(col(c).isNull().cast("int")).alias(c) for c in df.columns)).collect() #returns a list
-#    
-#    for i in temp:
-#        
-#        print(i)
-#
-#
-#print(missing_values())
-#
-
-
-#
-
-"""      IMPO       """
-#df.select(df['_id'], df['time'],df['heart_bpm'], df['v_fan'] ).show()
-#selected_df = backup_df.select(df['_id'], df['time'],df['heart_bpm'], df['v_fan'] )
-#selected_df.show()
-#
-##df.select(df['_id'], df['time'],df['heart_bpm'], df['v_fan'] ).show()
-#selected_df = backup_df.select(df['_id'], df['time'],df['heart_bpm'], df['v_fan'] )
-##selected_df.show()
-#
-#web_dataframe = selected_df.withColumn('time', functions.round(selected_df['time'], 2))        
-#
-#
-#
-#
-#
-#
-#type(web_dataframe)
-#
-#
-#selected_df = web_dataframe.select(web_dataframe['_id'], 
-#                                   web_dataframe['time'],
-#                                   web_dataframe['heart_bpm'],
-#                                   web_dataframe['v_fan']).toPandas()
-#
-
-
-
-#
-#web_dataframe = selected_df.withColumn('time', functions.round(selected_df['time'], 2))        
-#
-#
-#
-#
-#
-#
-#type(web_dataframe)
-#
-#
-#selected_df = web_dataframe.select(web_dataframe['_id'], 
-#                                   web_dataframe['time'],
-#                                   web_dataframe['heart_bpm'],
-#                                   web_dataframe['v_fan']).toPandas()
-#
-#
-#
-#
-#@flask_app.route('/')
-#def hello():
-#    
-#    return("Welcome to our project")
-#from matplotlib.figure import Figure
-#
-#@flask_app.route('/visualization1', methods=["GET","POST"])
-#def chart_heart_rate():
-#    
-#    figure = go.Figure()
-#
-#    figure.add_trace(go.Scatter(y = selected_df['heart_bpm'], x = selected_df['time']))
-#    
-#    figure.update_layout(title_text="Heart rate against time", xaxis_title = "Heart Rate" , yaxis_title = "Time")
-#    
-#    output = opy.plot(figure, auto_open=True, output_type='div')
-#
-#    return flask.render_template("index.html", output = output)
-#
-#
-#
-##how do you scale out - containerization
-#
-#if __name__ == "__main__":
-#    
-#    flask_app.run(debug=True, use_reloader=False)
-#
-#
-#
-#
-#
-#
-#
